3ec548a7c046ed92f04d617b825ed901dcf2035f
vasu-agarwal1 <157166784+vasu-agarwal1@users.noreply.github.com>
2025-06-28 12:22:23 +0530

    Initial commit

---
+++ src/components/Input.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Input(props){
+    return(
+    <input type="text" placeholder="Username" />
+    <input type="password" placeholder="Password" />
+    <input type="password" placeholder="Confirm Password" />
+    )
+}
+
+export default Input;
